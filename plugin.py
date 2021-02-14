3e3ff23b8218449376de732af7bb079a20b8f166
Tom Munnecke <munnecke@gmail.com>
2021-02-14 12:22:11 -0800

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -1,4 +1,24 @@
-import sys, json, threading;
+import sys, json, threading, time;
+
+import board, busio, adafruit_bno055; #these import modules are from Adafruit
+ 
+# Use these lines for I2C
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = adafruit_bno055.BNO055_I2C(i2c)
+last_val = 0xFFFF
+ 
+while True:
+    print("Temperature: {} degrees C".format(sensor.temperature))
+    print("Accelerometer (m/s^2): {}".format(sensor.acceleration))
+    print("Magnetometer (microteslas): {}".format(sensor.magnetic))
+    print("Gyroscope (rad/sec): {}".format(sensor.gyro))
+    print("Euler angle: {}".format(sensor.euler))
+    print("Quaternion: {}".format(sensor.quaternion))
+    print("Linear acceleration (m/s^2): {}".format(sensor.linear_acceleration))
+    print("Gravity (m/s^2): {}".format(sensor.gravity))
+    print()
+ 
+    time.sleep(3)
 
 n = 0
 def outputSk():
