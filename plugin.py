fc88f807e3dd6e93d12303e75c5663782310bfb9
Tom Munnecke <munnecke@gmail.com>
2021-02-15 14:48:14 -0800

    Update plugin.py
    added JSON for sensor values

--- plugin.py
+++ plugin.py
@@ -1,30 +1,21 @@
-import sys, json, threading, time;
+#BMO055 plugin for SignalK https://github.com/munnecke/sk-plugin-BMO055
 
+import sys, json, threading, time;
 import board, busio, adafruit_bno055; #these import modules are from Adafruit
- 
-# Use these lines for I2C
+
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_bno055.BNO055_I2C(i2c)
 last_val = 0xFFFF
- 
-while True:
-    print("Temperature: {} degrees C".format(sensor.temperature))
-    print("Accelerometer (m/s^2): {}".format(sensor.acceleration))
-    print("Magnetometer (microteslas): {}".format(sensor.magnetic))
-    print("Gyroscope (rad/sec): {}".format(sensor.gyro))
-    print("Euler angle: {}".format(sensor.euler))
-    print("Quaternion: {}".format(sensor.quaternion))
-    print("Linear acceleration (m/s^2): {}".format(sensor.linear_acceleration))
-    print("Gravity (m/s^2): {}".format(sensor.gravity))
-    print()
- 
-    time.sleep(3)
-
 n = 0
-def outputSk():
+def outputSk():  # retreive data from the sensor and put into JSON forma
     global n
-    skData = {'updates': [{ 'values': [{'path': 'some.path', 'value': n}]}]}
-    sys.stdout.write(json.dumps(skData) + '\n' + json.dumps(skData))
+    temp = "'values': {'path': 'environment/temperature','value': " + str(sensor.temperature) + " }"
+    bear = "'values': {'path': 'environment/compass','value': " + str(sensor.euler[0]) + " }"
+    heel = "'values': {'path': 'environment/heel','value': " + str(sensor.euler[1]) + " }"
+    pitch = "'values': {'path': 'environment/pitch','value': " + str(sensor.euler[2]) + " }"
+    skData = "{'updates':{" +temp+bear+heel+pitch+ "}"
+    
+sys.stdout.write(json.dumps(skData) + '\n' + json.dumps(skData))
     sys.stdout.write('\n')
     sys.stdout.flush()
     n += 1
@@ -39,4 +30,3 @@ for line in iter(sys.stdin.readline, b''):
     except:
         sys.stderr.write('error parsing json\n')
         sys.stderr.write(line)
-
